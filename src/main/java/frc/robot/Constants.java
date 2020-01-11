a5314ae2bb145c58a12f8a06b6ed88124d3a8e84
minemax03 <minemax03@gmail.com>
2020-01-11 13:41:28 -0500

    constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+public class Constants { 
+
+    //for distance calculations
+    public static double portHeight = 8.01875; //ft
+    public static double camHeight;
+    public static double camAngle;
+
+}
