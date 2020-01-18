6dd4ae0fee610c940df033a8c3ff194f5f466311
Elijah <sargeantfishcoc@gmail.com>
2020-01-18 17:20:02 -0500

    Adding in launcher equations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,12 +12,31 @@ public class Constants {
     //Drive subsystem constants
     public static final int DRIVE_FRONT_LEFT_SPEED_ID = 0;
     public static final int DRIVE_FRONT_LEFT_AZIMUTH_ID = 0;
+    public static final double FL_P = 0;
+    public static final double FL_I = 0;
+    public static final double FL_D = 0;
+    public static final int FL_AZIMUTH_ENCODER_ID = 0;
+
     public static final int DRIVE_FRONT_RIGHT_SPEED_ID = 0;
     public static final int DRIVE_FRONT_RIGHT_AZIMUTH_ID = 0;
+    public static final double RL_P = 0;
+    public static final double FR_I = 0;
+    public static final double FR_D = 0;
+    public static final int FR_AZIMUTH_ENCODER_ID = 0;
+
     public static final int DRIVE_BACK_LEFT_SPEED_ID = 0;
     public static final int DRIVE_BACK_LEFT_AZIMUTH_ID = 0;
+    public static final double BL_P = 0;
+    public static final double BL_I = 0;
+    public static final double BL_D = 0;
+    public static final int BL_AZIMUTH_ENCODER_ID = 0;
+
     public static final int DRIVE_BACK_RIGHT_SPEED_ID = 0;
     public static final int DRIVE_BACK_RIGHT_AZIMUTH_ID = 0;
+    public static final double BR_P = 0;
+    public static final double BR_I = 0;
+    public static final double BR_D = 0;
+    public static final int BR_AZIMUTH_ENCODER_ID = 0;
 
     //Shooter subsystem constants
     public static final int SHOOTER_ID = 0;
@@ -25,6 +44,8 @@ public class Constants {
     public static final double SHOOTER_FLYWHEEL_KD = 0; 
     public static final double SHOOTER_FLYWHEEL_KF = 0; 
     public static final double PORT_HEIGHT = 8.01875; //ft
+    public static final double SHOOTER_HEIGHT = 2;    //ft
+    public static final double SHOOTER_WHEEL_DIAMTER = 1/3; //ft
     //public static final double CAM_HEIGHT;
     //public static final double CAM_ANGLE;
 
--- src/main/java/frc/robot/subsytems/Shooter.java
+++ src/main/java/frc/robot/subsytems/Shooter.java
@@ -8,9 +8,11 @@
 package frc.robot.subsytems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
 import frc.robot.Constants;
 
 /**
@@ -21,22 +23,99 @@ public class Shooter {
     Vision vision;
 
     CANSparkMax shootMotor;
+    CANPIDController RPMController;
+    CANEncoder RPMEncoder;
 
     double speed;
     double angle;
-    double distance;
+    double distanceLimelight;
 
-    PIDController speedPID;
+    double height;
+    double distance;
+    double slope_constant;
+    double tanLineSlope;
+    double theta;
+    double velocity;
+    double thetaDegrees;
 
     public void init() {
         vision = new Vision();
         shootMotor = new CANSparkMax(Constants.SHOOTER_ID, MotorType.kBrushless);
+
+
+        RPMController = shootMotor.getPIDController();
+        RPMEncoder    = shootMotor.getEncoder();
+
+        RPMController.setP(Constants.SHOOTER_FLYWHEEL_KP);
+        RPMController.setD(Constants.SHOOTER_FLYWHEEL_KD);
+        RPMController.setFF(Constants.SHOOTER_FLYWHEEL_KF);
+        RPMController.setOutputRange(-1, 1);
+    }
+
+    /**
+     * Sets the launcher's hood to an angle
+     * @param hoodAngle the angle setpoint (in radians)
+     */
+    public void setAngle(double hoodAngle){
+    }
+
+    /**Sets the launcher's RPM
+     * @param velocityRPM The setpoint velocity for the power cell
+     */
+    public void setRPM(double velocityRPM){
+
+        double RPM = 60 * (velocity * Constants.SHOOTER_WHEEL_DIAMTER) / Math.PI;
+
+        RPMController.setReference(RPM, ControlType.kVelocity);
     }
 
     public void autoAimAtTarget() { //Distance, angle, and velocity
         //angle = vision.getVerticalOffset() + Constants.CAM_ANGLE;
-        //distance = (Constants.PORT_HEIGHT - Constants.CAM_HEIGHT) / Math.tan(angle);
+        //distanceLimelight = (Constants.PORT_HEIGHT - Constants.CAM_HEIGHT) / Math.tan(angle);
 
+        double[] setpoints = getSetpoints(distanceLimelight);
+
+        double angleSetpoint    = setpoints[0];
+        double velocitySetpoint = setpoints[1];
+
+        setAngle(angleSetpoint);
+        setRPM(velocitySetpoint);
+    }
+
+    /**
+     * Returns the launcher angle and exit velocity based on the distance
+     * @apiNote
+     * index 0 = angle (in radians) 
+     * @apiNote
+     * index 1 = velocity (in ft/s)
+     * 
+     * @param distance The distance from the port. (Use limelight generated distance)
+     */
+    public double[] getSetpoints(double distance) {
+
+        //delcaring our distances as the same so no weird mixups
+        this.distance = distance;
+
+        //getting adjusted height
+        height = (Constants.PORT_HEIGHT - Constants.SHOOTER_HEIGHT);
+
+        //grabbing the slope constant to calculate tangent line with
+        slope_constant = height / (distance * distance);
+
+        tanLineSlope = 2 * distance * slope_constant;
+
+        //The needed launcher velocity
+        theta = Math.atan(tanLineSlope);
+
+        velocity = distance / (Math.cos(theta) * Math.sqrt((height - (Math.tan(theta) * distance)) / -4.9));
+
+        thetaDegrees = Math.toDegrees(theta);
+
+        System.out.printf("Shooter angle: %f \nVelocity: %f", thetaDegrees, velocity);
+
+        double[] setpoints = {theta, velocity};
+
+        return setpoints;
     }
 
 }
