95b139e31073098c5da8f377ae914f9c7a3f5e9c
Elijah <sargeantfishcoc@gmail.com>
2020-03-08 14:39:50 -0400

    This is what vision was meant to exist as, logi

--- src/main/java/frc/robot/auto/actions/LineUpTowardsTargetWithDriver.java
+++ src/main/java/frc/robot/auto/actions/LineUpTowardsTargetWithDriver.java
@@ -10,6 +10,7 @@ package frc.robot.auto.actions;
 import frc.robot.Constants;
 import frc.robot.DriverController;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.NavX;
 import frc.robot.subsystems.Vision;
 
 /**
@@ -20,30 +21,42 @@ public class LineUpTowardsTargetWithDriver implements Action {
     DriverController driverController = DriverController.getInstance();
     Drive drive = Drive.getInstance();
     Vision vision = Vision.getInstance();
+    NavX gyro = NavX.getInstance();
 
-    double lastError = 0;
+    //double lastError = 0;
+
+    double targetAngle;
+    double error;
 
 
     public void start() {
 
         vision.lightOn();
+
+        targetAngle = gyro.getAngle() - vision.getHorizontalOffset();
     }
 
 
     public boolean loop() {
 
-        double error = 0 - vision.getHorizontalOffset();
-        double d = (error - lastError) / .04;
-        drive.move(driverController.getForward(), driverController.getStrafe(), vision.getHorizontalOffset() * Constants.AUTO_ROTATE_P + d * Constants.AUTO_ROTATE_D, 0, true);
+        error = gyro.getAngle() - targetAngle;
 
-        lastError = error;
+        drive.move(driverController.getForward(), driverController.getStrafe(), error * Constants.AUTO_ROTATE_P, 0, true);
+
+        //double error = 0 - vision.getHorizontalOffset();
+        //double d = (error - lastError) / .04;
+
+        
+        //drive.move(driverController.getForward(), driverController.getStrafe(), vision.getHorizontalOffset() * Constants.AUTO_ROTATE_P + d * Constants.AUTO_ROTATE_D, 0, true);
+
+        //lastError = error;
         return true; 
     }
 
     
     public void stop() {
 
-        lastError = 0;
+        //lastError = 0;
         vision.lightOff();
     }
 }
