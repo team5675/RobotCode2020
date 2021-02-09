59f9fe37e0338842ece4a19c7bcb65e06cdafde7
Max Berkowitz <maxberkowitz03@gmail.com>
2021-02-09 18:42:41 -0500

    fixing mabsballs

--- src/main/java/frc/robot/auto/MabsBalls.java
+++ src/main/java/frc/robot/auto/MabsBalls.java
@@ -24,24 +24,25 @@ drive forward 5 feet to c3
 public class MabsBalls {
 
 	Drive drive = new Drive();
+
+	double ySpeed;
+	static final double slowFactor = 1; //between 0 and 1
 	
 
 
-public void moveBitch(){
-	double ySpeed = 1;
-	double flEnc = drive.getFrontLeft().getSpeedPosition();
-	double frEnc = drive.getFrontRight().getSpeedPosition();
-	double blEnc = drive.getBackLeft().getSpeedPosition();
-	double brEnc = drive.getBackRight().getSpeedPosition();
-	double avg = ((flEnc + frEnc +blEnc + brEnc )/4);
-	ySpeed = 5/(avg) - 1;
-	drive.move(0, ySpeed, 0,  0, true);
-}
+	public void move5FeetBitch(){
+		double flEnc = drive.getFrontLeft().getSpeedPosition();
+		double frEnc = drive.getFrontRight().getSpeedPosition();
+		double blEnc = drive.getBackLeft().getSpeedPosition();
+		double brEnc = drive.getBackRight().getSpeedPosition();
+		double avg = ((flEnc + frEnc +blEnc + brEnc)/4);
+		ySpeed = slowFactor*((avg*avg / -25) + 1); //just a quadratic that is 1 at x = 0 and 0 at x = 5 for mapping
+		drive.move(0, ySpeed, 0,  0, true);
+	}
 
-public void uhhhhh(){
-
-
-}	
+	public void uhhhhh(){
+		
+	}	
 
 
 }
