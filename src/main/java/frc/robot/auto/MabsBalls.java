0377973841a375dcbab0dad660e3a3cc5df8ab80
Genevieve Woodby <ggwoodby@gmail.com>
2021-02-11 19:54:34 -0500

    mabbbly

--- src/main/java/frc/robot/auto/MabsBalls.java
+++ src/main/java/frc/robot/auto/MabsBalls.java
@@ -3,8 +3,11 @@ import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Sucker;
 import frc.robot.auto.Pathfinder;
 import frc.robot.Constants;
+import frc.robot.auto.Waypoint;
 
 import com.fasterxml.jackson.databind.node.ContainerNode;
+
+import org.apache.commons.math3.ode.FirstOrderFieldIntegrator;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
@@ -27,26 +30,64 @@ drive forward 5 feet to c3
 
 
 public class MabsBalls {
-
+	
 	Waypoint[] firstPointsRed, firstPointsBlue, secondPointsRed, secondPointsBlue;
+	//FirstOrderFieldIntegrator
 
 	Pathfinder pathfinder;
 	Drive drive;
 	Sucker sucker;
 
 	double ySpeed;
+	double ftMoved;
 	static final double slowFactor = 1; //between 0 and 1
 
 	boolean isFirstMap;
 	boolean cont;
 	boolean pathA;
 	boolean pathB;
-	double ftMoved;
+	
 
 	public MabsBalls() {
 		cont = true;
 		pathA = false;
 		pathB = false;
+
+		Waypoint[] firstPointsRed = new Waypoint[1];
+		Waypoint[] firstPointsBlue = new Waypoint[1];
+		Waypoint[] secondPointsRed = new Waypoint[1];
+		Waypoint[] secondPointsBlue = new Waypoint[1];
+		
+
+		firstPointsRed = new Waypoint[] {
+
+			new Waypoint(12.5, 5, 0),
+			new Waypoint(15,12.5,0),
+			new Waypoint(27.5, 7.5,0)
+		};
+
+		firstPointsBlue = new Waypoint[] {
+
+			new Waypoint(15.5, 2.5, 0),
+			new Waypoint(17.5,10,0),
+			new Waypoint(22.5,7.5,0),
+			new Waypoint(27.5,7.5,0)
+		};
+		
+		secondPointsRed = new Waypoint[] {
+
+			new Waypoint(7.5, 10, 0),
+			new Waypoint(12.5,10,0),
+			new Waypoint(17.5, 10,0),
+		};
+		secondPointsBlue = new Waypoint[] {
+
+			new Waypoint(15.5, 5, 0),
+			new Waypoint(20,10,0),
+			new Waypoint(25,10,0),
+		};
+		
+
 
 		drive = Drive.getInstance();
 		sucker = Sucker.getInstance();
@@ -55,7 +96,7 @@ public class MabsBalls {
 	
 
 
-	public void move5FeetBitch(){
+	public void moveOneAndAHalfFeetBitch(){
 		double flEnc = drive.getFrontLeft().getSpeedPosition();
 		double frEnc = drive.getFrontRight().getSpeedPosition();
 		double blEnc = drive.getBackLeft().getSpeedPosition();
@@ -74,7 +115,7 @@ public class MabsBalls {
 
 		sucker.suckOrBlow(-1); //or whatever speed value we want it at
 
-		if(cont) move5FeetBitch();
+		if(cont) moveOneAndAHalfFeetBitch();
 
 		sucker.ballIn(); //basically just keeps rechecking if a ball is found
 
@@ -110,4 +151,5 @@ public class MabsBalls {
 		}
 	}	
 
+	
 }
