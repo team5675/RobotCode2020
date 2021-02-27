895907f70069eba260a1f6e8faa3b900373916ca
Genevieve Woodby <ggwoodby@gmail.com>
2021-02-26 22:54:16 -0500

    i tried

--- src/main/java/frc/robot/auto/PathWeezer.java
+++ src/main/java/frc/robot/auto/PathWeezer.java
@@ -2,21 +2,17 @@
 
 package frc.robot.auto;
 
-import frc.robot.subsystems.*;
+import java.io.File;
 import java.util.Scanner;
-import frc.libs.swerve.*;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 
 public class PathWeezer {
 
     // get
     // send
 
-    private Subsystem drive = WheelDrive.getInstance();
+  //private Subsystem drive = drive.getInstance();
     
     public static Scanner in;
     File pathWeaver;
@@ -25,59 +21,52 @@ public class PathWeezer {
     final static double I = 0.1;
     final static double D = 0.1;
     
-    final static int SEGMENTS; //obv not the real number
+    final static int SEGMENTS = setSegments(); // obv not the real number
     final static int VARIABLES = 6;
     File path = new File("bluea.wpilib.json");
-    private static double[] currentSegment; // [x, y, angle, and whatever else longman send mee]
+   // private static double[] currentSegment; // [x, y, angle]
     static double[][] trajectory = new double[SEGMENTS][VARIABLES];
     
     public PathWeezer() {
-           
 
         try {
     
-            in = new Scanner(path);
+           Scanner in = new Scanner(path);
     
         } catch (FileNotFoundException e) {
     
             e.printStackTrace();
         }
+            
     }
 
-        
-    
-    
+     public static void setTrajectory(){
 
 
-       /* public void loop() { //change this name
-    
-            for(double[] segment : trajectory) {
-    
-            }
-    */
-        
-        
-        
-    public static void setTrajectoryArray() {
-    
-        for (int i = 0; i < SEGMENTS; i++) {
-    
-            String[] data = in.nextLine().split(",");
-    
-            for (int k = 0; k < VARIABLES; k++) {
-    
-                trajectory[i][k] = Double.parseDouble(data[k]);
-            }
+        for(int a = 0; a < SEGMENTS; a++){
+
+            for (int x = 0; x < VARIABLES; x++){
+                  trajectory[a][x] = in.nextDouble(); 
+
+               } 
+           }
         }
-    }
+        public static void setSegments() {
+            while (in.hasNext()) {
+                int doubles = 0;
+                String var = in.next();
+                try{
+                   // it double
+                    double d = Double.parseDouble(var);
+                     doubles += 1;
+                    
+                 }
+                 catch (NumberFormatException e) {
+                    //not double
+                    doubles = doubles;
+                }
+            
+                return doubles;
+         }
+        }
 }
-    
-    
-
-
-
-
-
-
-
-
