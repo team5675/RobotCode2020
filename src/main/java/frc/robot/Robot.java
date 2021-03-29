2dd7bb4c53dc2d004567d9cde12e850424277a05
Genevieve Woodby <ggwoodby@gmail.com>
2021-03-29 19:18:54 -0400

    matt hummel time

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -80,6 +80,17 @@ public class Robot extends TimedRobot {
 
     //actionRunner.start();
     //modeRunner.start();
+
+    // ode to matt hummel
+    //drive.move(, strafe, rotation, angle, isFieldOriented);
+    drive.move(.5, .5, 0, 0, true);
+    try {
+      Thread.sleep(1000);
+  } catch (InterruptedException e) {
+      e.printStackTrace();
+  }
+  drive.move(0, 0, 0, 0, true);
+
   }
 
 
